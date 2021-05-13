a8ae3beb4374848b016c4fa44e8b414d43af3ff5
sibaprksh <siba4269@gmail.com>
2021-05-13 16:55:32 +0530

    3 files modified, 1 added, 1 deleted

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -1,4 +1,7 @@
 import authReducer from './reducer';
 import { authActions } from './actions';
 
-export { authReducer, authActions };
+import { Login } from './Login';
+import { Register } from './Register';
+
+export { authReducer, authActions, Login, Register };
